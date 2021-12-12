d1f28bec855ba4f27fdde609c2546c31f3aa5306
Wafelowski <heavywolfpl@gmail.com>
2021-12-12 20:28:25 +0100

    rebuild for exiled 4.0

--- SupplyDrop/Plugin.cs
+++ SupplyDrop/Plugin.cs
@@ -17,7 +17,7 @@ namespace SupplyDrop
         public override string Name { get; } = "SupplyDrop";
         public override string Prefix { get; } = "SD";
         public override Version Version { get; } = new Version(3, 0, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(4, 0, 0);
 
         internal static SupplyDrop Singleton;
 
